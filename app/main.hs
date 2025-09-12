fc2446598bd826acbb5f98c6b42691cc017f7580
haruki7049 <tontonkirikiri@gmail.com>
2025-09-12 20:42:20 +0900

    Update

--- app/main.hs
+++ app/main.hs
@@ -1,5 +1,18 @@
 module Main where
 
+import Text.Parsec
+import Text.Parsec.Expr
+import qualified Text.Parsec.Token as TT
+import qualified Text.Parsec.Language as Lang
+
+type Parser a = Parsec String () a
+
+natural :: Parser Integer
+natural = TT.natural Lang.haskell
+
 main :: IO ()
 main = do
-  putStrLn "Hi"
+  s <- getLine
+  case parse natural "stdin" s of
+    Left err -> print err
+    Right x -> print x
