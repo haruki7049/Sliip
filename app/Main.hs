0ac62f41c479d5407c267b854cf1d0ba98a56eb6
haruki7049 <tontonkirikiri@gmail.com>
2025-09-13 20:37:10 +0900

    feat(Main.hs): Create CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,30 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 import qualified Sliip (programs)
 import Text.Parsec (parse)
+import Options.Applicative (Parser, ParserInfo, switch, info, progDesc, helper, (<**>), help, short, long, metavar, strOption, execParser)
 
 main :: IO ()
-main = do
-  putStrLn "Enter your SExpression..."
+main = execParser argumentParserInfo >>= run
 
-  s <- getLine
-  case parse Sliip.programs "stdin" s of
+run :: CLIArgument -> IO ()
+run CLIArgument {..} = do
+  case parse Sliip.programs "" script of
     Left err -> print err
     Right x -> print x
+
+data CLIArgument = CLIArgument
+  { script :: String
+  } deriving (Read, Show)
+
+cliParser :: Parser CLIArgument
+cliParser = CLIArgument
+  <$> strOption (long "script" <> short 's' <> metavar "SCRIPT" <> help "Write your Sliip script here")
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo p = info (p <**> helper) . progDesc
+
+argumentParserInfo :: ParserInfo CLIArgument
+argumentParserInfo = cliParser `withInfo` "A joke Lisp interpreter"
