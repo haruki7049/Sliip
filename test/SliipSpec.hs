9f66989141e6245ac01eb91cf392123e08abb48c
haruki7049 <tontonkirikiri@gmail.com>
2025-09-13 14:21:18 +0900

    Add tests

---
+++ test/SliipSpec.hs
@@ -0,0 +1,26 @@
+module SliipSpec (spec) where
+
+import Sliip (sexpr)
+import Test.Hspec (Spec, describe, it, shouldSatisfy)
+import Text.Parsec (parse)
+
+spec :: Spec
+spec = do
+  describe "parse" $ do
+    it "parses a simple SExpression" $ do
+      let input = "( hoge )"
+          result = parse Sliip.sexpr "" input
+      result
+        `shouldSatisfy` ( \r -> case r of
+                            Left _ -> False
+                            Right _ -> True
+                        )
+
+    it "parses a nested SExpression" $ do
+      let input = "( hoge ( fuga piyo ) )"
+          result = parse Sliip.sexpr "" input
+      result
+        `shouldSatisfy` ( \r -> case r of
+                            Left _ -> False
+                            Right _ -> True
+                        )
