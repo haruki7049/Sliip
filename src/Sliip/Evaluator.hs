b367a73bc9ab8443c575853d8b6c56821bc59296
haruki7049 <tontonkirikiri@gmail.com>
2025-09-21 10:45:22 +0900

    Update

--- src/Sliip/Evaluator.hs
+++ src/Sliip/Evaluator.hs
@@ -26,6 +26,7 @@ newtype Statement = WriteLine String
 data EvaluationError
   = NoMainFound
   | InvalidForm SExpression
+  | InvalidAtom Atom
   | UnknownBuitin String
   | UnknownReference String
   deriving (Show, Eq)
@@ -34,6 +35,7 @@ data Value
   = VString String
   | VLambda [String] SExpression Environment
   | VBuiltin String (Value -> Either EvaluationError Value)
+  | VThunk Atom Environment
 
 instance Show Value where
   show (VString s) = "VString " ++ show s
@@ -71,22 +73,47 @@ evalPrograms p = do
 buildEnv :: Programs -> Environment
 buildEnv = foldl insertDef empty
   where
-    insertDef env (SExpr [Builtin "define", Reference name, StringLiteral s]) =
-      insert name (VString s) env
+    insertDef :: Environment -> SExpression -> Environment
+    insertDef env (SExpr [Builtin "define", Reference name, expr]) =
+      insert name (VThunk expr env) env
     insertDef env _ = env
 
 lookupVar :: String -> Environment -> Either EvaluationError Value
 lookupVar name env =
   case lookup name env of
+    Just (VThunk sexpr closureEnv) -> do
+      val <- evalValue closureEnv sexpr
+      Right val
     Just v -> Right v
     Nothing -> Left (UnknownReference name)
 
 evalSExpr :: Environment -> SExpression -> Either EvaluationError Executable
 evalSExpr env (SExpr [Builtin "define", Builtin "main", SExprV sexpr]) = evalSExpr env sexpr
-evalSExpr env (SExpr [Builtin "lambda", SExprV args, SExprV sexpr]) = evalLambda env args sexpr
+evalSExpr env (SExpr [Builtin "lambda", SExprV args, SExprV body]) =
+  case buildLambda args body env of
+    Right (VLambda a b e) -> evalLambda e a b
+    Left err -> Left err
+evalSExpr env (SExpr (fnExpr : argExprs)) =
+  do
+    fnVal <- evalValue env fnExpr
+    argVals <- mapM (evalValue env) argExprs
+    apply fnVal argVals
 evalSExpr _ sexpr = Left (InvalidForm sexpr)
 
-evalLambda :: Environment -> SExpression -> SExpression -> Either EvaluationError Executable
+buildLambda :: SExpression -> SExpression -> Environment -> Either EvaluationError Value
+buildLambda (SExpr args) body env =
+  let argNames = [name | Reference name <- args]
+   in Right (VLambda argNames body env)
+buildLambda _ _ _ = Left (InvalidForm (SExpr []))
+
+evalValue :: Environment -> Atom -> Either EvaluationError Value
+evalValue _ (StringLiteral s) = Right (VString s)
+evalValue env (Reference ref) = lookupVar ref env
+evalValue env (SExprV (SExpr [Builtin "lambda", SExprV args, SExprV body])) =
+  buildLambda args body env
+evalValue _ atom = Left (InvalidAtom atom)
+
+evalLambda :: Environment -> [String] -> SExpression -> Either EvaluationError Executable
 evalLambda env _ (SExpr [Builtin "write-line", StringLiteral stringLiteral]) = Right [WriteLine stringLiteral]
 evalLambda env _ (SExpr [Builtin "write-line", Reference ref]) =
   case lookupVar ref env of
@@ -95,6 +122,14 @@ evalLambda env _ (SExpr [Builtin "write-line", Reference ref]) =
     Left err -> Left err
 evalLambda _ _ (SExpr []) = Right []
 evalLambda _ _ sexpr = Left (InvalidForm sexpr)
+
+-- Apply lambda
+apply :: Value -> [Value] -> Either EvaluationError Executable
+apply (VLambda params body closureEnv) args
+  | length params == length args =
+      let localEnv = foldl (\e (p, a) -> insert p a e) closureEnv (zip params args)
+       in evalLambda localEnv params body
+  | otherwise = Left (InvalidForm body)
 
 getMain :: Programs -> Maybe SExpression
 getMain = find (\expr -> isDefine expr && isMain expr)
