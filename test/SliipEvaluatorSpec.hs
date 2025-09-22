5fce8d11f77cd3de80c97e0077d815a49cbce9cf
haruki7049 <tontonkirikiri@gmail.com>
2025-09-22 18:30:04 +0900

    test(SliipEvaluatorSpec): Add tests about eval function

--- test/SliipEvaluatorSpec.hs
+++ test/SliipEvaluatorSpec.hs
@@ -8,6 +8,28 @@ import System.IO.Silently (capture_)
 spec :: Spec
 spec = do
   describe "eval" $ do
+    it "can reads define after main definition" $ do
+      let program = unlines
+            [ "(define main"
+            , "  (lambda ()"
+            , "    (write-line fooString)))"
+            , ""
+            , "(define fooString \"foo\")"
+            ]
+      out <- capture_ (eval program)
+      out `shouldBe` "foo\n"
+
+    it "can reads define before main definition" $ do
+      let program = unlines
+            [ "(define fooString \"foo\")"
+            , ""
+            , "(define main"
+            , "  (lambda ()"
+            , "    (write-line fooString)))"
+            ]
+      out <- capture_ (eval program)
+      out `shouldBe` "foo\n"
+
     it "prints result of main's write-line" $ do
       let program = unlines
             [ "(define main"
